6adda638ca83d24fa9bdfbe07fdc17d9ac404f87
icazz <icha.zika.1@gmail.com>
2026-01-12 15:43:32 +0700

    Final fix for static assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Vite; // <--- JANGAN LUPA BARIS INI
+use Illuminate\Support\ServiceProvider;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -12,8 +15,14 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-
+        // KITA UBAH KE SINI:
+        // Karena di screenshot kamu filenya ada di "public/build/manifest.json"
+        // Kita suruh Laravel pakai nama file itu saja, tanpa embel-embel ".vite"
+        Vite::useManifestFilename('manifest.json');
     }
 }
